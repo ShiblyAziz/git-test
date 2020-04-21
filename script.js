20b80ee8585965212c612258c8f57b7bfb218689
Shibly Aziz <shiblyaziz@Shiblys-MacBook-Pro.local>
2020-04-21 16:22:59 +0200

    comment added

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var numbers = [1,2,3,4,5];
+
+// routine to calculate the sum
 function sum (n){
 	var summation =0;
 	// do the actual sum
  for (i= 0; i<n; length; i++) { summation += n[i] }
- 	
 	return summation;
 
 }
